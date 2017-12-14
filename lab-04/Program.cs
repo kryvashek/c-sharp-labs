a9535f05b23e08195e7f28b63fa7cfffaaaae8f5
kryvashek <kryvashek@gmail.com>
2017-12-14 21:29:41 +0200

    Implemented summing/substracting of two vectors.
    Signed-off-by: kryvashek <kryvashek@gmail.com>

---
+++ lab-04/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace lab04 {
+    class MainClass {
+        public static void Main( string[] args ) {
+            Console.WriteLine( "Hello World!" );
+
+            Vector< int >   a = new Vector<int>( 1, 2, 3, 4, 5 ),
+                            b = new Vector<int>( 10, 8, 6, 4, 2 );
+
+            foreach( int value in ( a + b ) )
+                Console.WriteLine( value );
+        }
+    }
+}
---
+++ lab-04/Vector.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using Microsoft.CSharp;
+
+namespace lab04 {
+
+    public class Vector< T > : IEnumerable< T >
+        where T : struct, IEquatable< T >, IComparable< T >, IFormattable {
+
+        T[] _values;
+
+        public Vector( int length ) {
+            _values = new T[ length ];
+        }
+
+        public Vector( params T[] values ) {
+            _values = values;
+        }
+
+        public int Length {
+            get {
+                return _values.GetLength( 0 );
+            }
+        }
+
+        public IEnumerator< T > GetEnumerator() {
+            return _values.Cast<T>().GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() {
+            return ( IEnumerator )GetEnumerator();
+        }
+
+        public int GetLength() {
+            return Length;
+        }
+
+        public static Vector< T > operator +( Vector< T > x, Vector< T > y ) {
+            if( x.Length != y.Length )
+                throw new ArrayTypeMismatchException( "Unequal lengths of summed vectors." );
+
+            Vector< T > z = new Vector<T>( x.Length );
+
+            for( int i = 0; i < x.Length; i++ )
+                z._values[ i ] = ( dynamic )x._values[ i ] + ( dynamic )y._values[ i ];
+
+            return z;
+        }
+
+        public static Vector< T > operator -( Vector< T > x, Vector< T > y ) {
+            if( x.Length != y.Length )
+                throw new ArrayTypeMismatchException( "Unequal lengths of subtracted vectors." );
+
+            Vector< T > z = new Vector<T>( x.Length );
+
+            for( int i = 0; i < x.Length; i++ )
+                z._values[ i ] = ( dynamic )x._values[ i ] - ( dynamic )y._values[ i ];
+
+            return z;
+        }            
+    }
+}
+
