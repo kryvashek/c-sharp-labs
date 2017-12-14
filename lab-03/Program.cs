edb0fbe8c1dd9c031bba44b973302ead7b1ef259
kryvashek <kryvashek@gmail.com>
2017-12-14 19:37:04 +0200

    Test Ratio usage implemented.
    Signed-off-by: kryvashek <kryvashek@gmail.com>

--- lab-03/Program.cs
+++ lab-03/Program.cs
@@ -3,7 +3,32 @@
 namespace lab03 {
     class MainClass {
         public static void Main( string[] args ) {
-            Console.WriteLine( "Hello World!" );
+            Ratio   a = new Ratio( 3, 5 ),
+                    b = new Ratio( 7, 21 ),
+                    c = new Ratio( 15, 196 );
+
+            Console.WriteLine( "Has fractions " +
+                a + " (" + a.toDouble + "), " +
+                b + " (" + b.toDouble + ") and " +
+                c + " (" + c.toDouble + ")" );
+
+            c += a;
+            b -= a;
+            a *= a;
+
+            Console.WriteLine( "Has fractions " +
+                a + " (" + a.toDouble + "), " +
+                b + " (" + b.toDouble + ") and " +
+                c + " (" + c.toDouble + ")" );
+
+            c /= b;
+            b = -b * a;
+            a = c * b;
+
+            Console.WriteLine( "Has fractions " +
+                a + " (" + a.toDouble + "), " +
+                b + " (" + b.toDouble + ") and " +
+                c + " (" + c.toDouble + ")" );
         }
     }
 }
