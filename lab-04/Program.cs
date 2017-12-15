a71b30927958f27b086deb02c1a024c51cd5c5ed
kryvashek <kryvashek@gmail.com>
2017-12-15 15:31:06 +0200

    Finished abstract vector implementation.

--- lab-04/Program.cs
+++ lab-04/Program.cs
@@ -2,14 +2,57 @@
 
 namespace lab04 {
     class MainClass {
+        public delegate void TAction< T >( T value );
+
+        public static void Foreach< T >( Vector< T > v, TAction< T > action ) where T : IEquatable< T >, IFormattable, new() {
+            foreach( T value in v )
+                action( value );
+        }
+
+        public static void PrintOne< T >( T value ) {
+            Console.Write( value + " " );
+        }
+
+        public static void PrintEach< T >( Vector< T > v ) where T : IEquatable< T >, IFormattable, new() {
+            Foreach( v, PrintOne< T > );
+            Console.WriteLine();
+        }
+
         public static void Main( string[] args ) {
-            Console.WriteLine( "Hello World!" );
+            Vector< double >   a = new Vector<double>( 1, 2, 3 ),
+                               b = new Vector<double>( 10, 8, 6 );
 
-            Vector< int >   a = new Vector<int>( 1, 2, 3, 4, 5 ),
-                            b = new Vector<int>( 10, 8, 6, 4, 2 );
+            Console.WriteLine( "Обход по индексам a" );
+            for( int i = 0; i < a.Length; i++ )
+                Console.Write( a[ i ] + " " );
+            Console.WriteLine();
+            
+            Console.WriteLine( "Обход по индексам b" );
+            for( int i = 0; i < b.Length; i++ )
+                Console.Write( b[ i ] + " " );
+            Console.WriteLine();
 
-            foreach( int value in ( a + b ) )
-                Console.WriteLine( value );
+            Console.WriteLine( "Сумма" );
+            PrintEach( a + b );
+
+            Console.WriteLine( "Разность" );
+            PrintEach( b - a );
+
+            Console.WriteLine( "Умножение на скаляр" );
+            PrintEach( b * 0.5 );
+
+            Console.WriteLine( "Скалярное произведение" );
+            Console.WriteLine( a * b );
+
+            Console.WriteLine( "Векторное произведение a | b" );
+            PrintEach( a | b );
+
+            Console.WriteLine( "Смешанное произведение b * (a | b)" );
+            Console.WriteLine( b * ( a | b ) );
+
+            Console.WriteLine( "Смешанное произведение (a | b) * a" );
+            Console.WriteLine( ( a | b ) * a );
+
         }
     }
 }
--- lab-04/Vector.cs
+++ lab-04/Vector.cs
@@ -5,11 +5,32 @@ using System.Linq;
 using Microsoft.CSharp;
 
 namespace lab04 {
-
-    public class Vector< T > : IEnumerable< T >
-        where T : struct, IEquatable< T >, IComparable< T >, IFormattable {
+    public class Vector< T > : IEnumerable< T >, IEquatable< Vector< T > >
+        where T : IEquatable< T >, new() {
 
         T[] _values;
+
+#region Properties
+
+        public int Length {
+            get {
+                return _values.GetLength( 0 );
+            }
+        }
+
+        public T this[ int i ] {
+            get {
+                return _values[ i ];
+            }
+
+            set {
+                _values[ i ] = value;
+            }
+        }
+
+#endregion
+
+#region Methods
 
         public Vector( int length ) {
             _values = new T[ length ];
@@ -19,14 +40,8 @@ namespace lab04 {
             _values = values;
         }
 
-        public int Length {
-            get {
-                return _values.GetLength( 0 );
-            }
-        }
-
         public IEnumerator< T > GetEnumerator() {
-            return _values.Cast<T>().GetEnumerator();
+            return _values.Cast< T >().GetEnumerator();
         }
 
         IEnumerator IEnumerable.GetEnumerator() {
@@ -37,7 +52,30 @@ namespace lab04 {
             return Length;
         }
 
-        public static Vector< T > operator +( Vector< T > x, Vector< T > y ) {
+        public bool Equals( Vector< T > ov ) {
+            if( null == ov )
+                return false;
+
+            if( Length != ov.Length )
+                return false;
+
+            for( int i = 0; i < Length; i++ )
+                if( ( dynamic )_values[ i ] != ( dynamic )ov._values[ i ] )
+                    return false;
+
+            return true;
+
+        }
+
+        public override bool Equals( Object o ) {
+            return Equals( o as Vector< T > );
+        }
+
+        public override int GetHashCode() {
+            return _values.GetHashCode();
+        }
+
+        public static Vector< T > operator +( Vector< T > x, Vector< T > y ) { // summ
             if( x.Length != y.Length )
                 throw new ArrayTypeMismatchException( "Unequal lengths of summed vectors." );
 
@@ -49,7 +87,7 @@ namespace lab04 {
             return z;
         }
 
-        public static Vector< T > operator -( Vector< T > x, Vector< T > y ) {
+        public static Vector< T > operator -( Vector< T > x, Vector< T > y ) { // substract
             if( x.Length != y.Length )
                 throw new ArrayTypeMismatchException( "Unequal lengths of subtracted vectors." );
 
@@ -59,7 +97,60 @@ namespace lab04 {
                 z._values[ i ] = ( dynamic )x._values[ i ] - ( dynamic )y._values[ i ];
 
             return z;
-        }            
+        }
+
+        public static T operator *( Vector< T > x, Vector< T > y ) { // scalar product
+            if( x.Length != y.Length )
+                throw new ArrayTypeMismatchException( "Unequal lengths of subtracted vectors." );
+
+            T r = new T();
+
+            for( int i = 0; i < x.Length; i++ )
+                r += ( dynamic )x._values[ i ] * ( dynamic )y._values[ i ];
+
+            return r;
+        }
+
+        public static Vector< T > operator *( Vector< T > x, T a ) { // post-coeff
+            Vector< T > z = new Vector<T>( x.Length );
+
+            for( int i = 0; i < x.Length; i++ )
+                z._values[ i ] = ( dynamic )x._values[ i ] * ( dynamic )a;
+
+            return z;
+        }
+
+        public static Vector< T > operator *( T a, Vector< T > x ) { // pre-coeff
+            Vector< T > z = new Vector<T>( x.Length );
+
+            for( int i = 0; i < x.Length; i++ )
+                z._values[ i ] = ( dynamic )a * ( dynamic )x._values[ i ];
+
+            return z;
+        }
+
+        public static Vector< T > operator |( Vector< T > x, Vector< T > y ) { // vector product
+            if( 3 != x.Length || 3 != y.Length )
+                throw new ArrayTypeMismatchException( "Cross product is available only for 3D." );
+
+            Vector< T > z = new Vector<T>( 3 );
+
+            z._values[ 0 ] = ( dynamic )x._values[ 1 ] * ( dynamic )y._values[ 2 ] - ( dynamic )x._values[ 2 ] * ( dynamic )y._values[ 1 ];
+            z._values[ 1 ] = ( dynamic )x._values[ 2 ] * ( dynamic )y._values[ 0 ] - ( dynamic )x._values[ 0 ] * ( dynamic )y._values[ 2 ];
+            z._values[ 2 ] = ( dynamic )x._values[ 0 ] * ( dynamic )y._values[ 1 ] - ( dynamic )x._values[ 1 ] * ( dynamic )y._values[ 0 ];
+
+            return z;
+        }
+
+        public static bool operator ==( Vector< T > x, Vector< T > y ) { // comparison
+            return x.Equals( y );
+        }
+
+        public static bool operator !=( Vector< T > x, Vector< T > y ) { // miscomparison
+            return !x.Equals( y );
+        }
+
+#endregion
     }
 }
 
