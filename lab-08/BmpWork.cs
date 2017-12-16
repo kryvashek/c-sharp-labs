3def18b4191ce4224481a8421306a5d9648d11d2
kryvashek <kryvashek@gmail.com>
2017-12-16 17:50:33 +0200

    Normalized BmpWork class.

--- lab-08/BmpWork.cs
+++ lab-08/BmpWork.cs
@@ -4,19 +4,39 @@ using System.Drawing;
 using System.Drawing.Imaging;
 
 namespace lab08 {
-    public class BitmapsWork {
-        public static Bitmap LoadBitmap(string fileName) {
-            using (FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.Read))
-                return new Bitmap(fs);
+    public class BmpWork {
+        static Bitmap LoadBitmap( string fileName ) {
+            try {
+                using( FileStream fs = new FileStream( fileName, FileMode.Open, FileAccess.Read, FileShare.Read ) )
+                    return new Bitmap( fs );
+            } catch( Exception e ) {
+                Console.WriteLine( "Failed loading image from file " + fileName + ":\n" + e );
+                return null;
+            }
         }
 
-        public static byte[][,] CreateRgbBitmap( int height, int width ) {
+        static bool SaveBitmap( ref Bitmap bmp, string fileName, ImageFormat format ) {
+            try {
+                bmp.Save( fileName, format );
+                return true;
+            } catch( Exception e ) {
+                Console.WriteLine( "Failed saving new image file " + fileName + ":\n" + e );
+                return false;
+            }
+        }
+
+        public static byte[][,] CreateImageBytes( int height, int width ) {
             return new byte[ 3 ][,] { new byte[ height, width ], new byte[ height, width ], new byte[ height, width ] };
         }
 
-        public unsafe static byte[][,] BitmapToBytes( Bitmap bmp ) {
+        public unsafe static byte[][,] LoadImageBytes( string fileName ) {
+            Bitmap bmp = LoadBitmap( fileName );
+
+            if( null == bmp )
+                return null;
+
             int width = bmp.Width, height = bmp.Height, h, w;
-            byte[][,] res = CreateRgbBitmap( height, width );
+            byte[][,] res = CreateImageBytes( height, width );
             BitmapData bd = bmp.LockBits( new Rectangle( 0, 0, width, height ), ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb );
 
             try {
@@ -33,10 +53,11 @@ namespace lab08 {
             finally {
                 bmp.UnlockBits( bd );
             }
+
             return res;
         }
 
-        public unsafe static Bitmap BytesToBitmap( byte[][,] rgb ) {
+        public unsafe static void SaveImageBytes( byte[][,] rgb, string fileName, string format ) {
             if( 3 != rgb.GetLength( 0 ) )
                 throw new ArrayTypeMismatchException( "Size of first dimension for passed array must be 3 (RGB components)." );
 
@@ -62,7 +83,23 @@ namespace lab08 {
                 res.UnlockBits( bd );
             }
 
-            return res;
+            SaveBitmap( ref res, fileName, Ext2Fmt( format ) );
+        }
+
+        public static string Fmt2Ext( ImageFormat format ) {
+            if( format.Equals( ImageFormat.Bmp ) ) return "bmp";
+            if( format.Equals( ImageFormat.Jpeg ) ) return "jpg";
+            if( format.Equals( ImageFormat.Png ) ) return "png";
+            return "tiff";
+        }
+
+        public static ImageFormat Ext2Fmt( string extension ) {
+            switch( extension ) {
+                case "bmp": return ImageFormat.Bmp;
+                case "jpg": return ImageFormat.Jpeg;
+                case "png": return ImageFormat.Png;
+                default: return ImageFormat.Tiff;
+            }
         }
     }
 }
