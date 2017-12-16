8ecef3ce452121d7a2b0cc8c899a476c94612a79
kryvashek <kryvashek@gmail.com>
2017-12-16 16:41:15 +0200

    Separated BitmapWorks class.

---
+++ lab-08/BitmapsWork.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace lab08 {
+    public class BitmapsWork {
+        public static Bitmap LoadBitmap(string fileName) {
+            using (FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.Read))
+                return new Bitmap(fs);
+        }
+
+        public static byte[][,] CreateRgbBitmap( int height, int width ) {
+            return new byte[ 3 ][,] { new byte[ height, width ], new byte[ height, width ], new byte[ height, width ] };
+        }
+
+        public unsafe static byte[][,] BitmapToBytes( Bitmap bmp ) {
+            int width = bmp.Width, height = bmp.Height, h, w;
+            byte[][,] res = CreateRgbBitmap( height, width );
+            BitmapData bd = bmp.LockBits( new Rectangle( 0, 0, width, height ), ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb );
+
+            try {
+                byte* curpos;
+                for( h = 0; h < height; h++ ) {
+                    curpos = ( ( byte * )bd.Scan0 ) + h * bd.Stride;
+                    for( w = 0; w < width; w++ ) {
+                        res[ 2 ][ h, w ] = *( curpos++ );
+                        res[ 1 ][ h, w ] = *( curpos++ );
+                        res[ 0 ][ h, w ] = *( curpos++ );
+                    }
+                }
+            }
+            finally {
+                bmp.UnlockBits( bd );
+            }
+            return res;
+        }
+
+        public unsafe static Bitmap BytesToBitmap( byte[][,] rgb ) {
+            if( 3 != rgb.GetLength( 0 ) )
+                throw new ArrayTypeMismatchException( "Size of first dimension for passed array must be 3 (RGB components)." );
+
+            if( 2 != rgb[ 0 ].Rank )
+                throw new ArrayTypeMismatchException( "Every passed component array should have 2 dimensions." );
+
+            int height = rgb[ 0 ].GetLength( 0 ), width = rgb[ 0 ].GetLength( 1 ), h, w;
+            Bitmap res = new Bitmap( width, height, PixelFormat.Format24bppRgb );
+            BitmapData bd = res.LockBits( new Rectangle( 0, 0, width, height ), ImageLockMode.WriteOnly, PixelFormat.Format24bppRgb );
+
+            try {
+                byte* curpos;
+                for( h = 0; h < height; h++ ) {
+                    curpos = ( ( byte * )bd.Scan0 ) + h * bd.Stride;
+                    for( w = 0; w < width; w++ ) {
+                        *( curpos++ ) = rgb[ 2 ][ h, w ];
+                        *( curpos++ ) = rgb[ 1 ][ h, w ];
+                        *( curpos++ ) = rgb[ 0 ][ h, w ];
+                    }
+                }
+            }
+            finally {
+                res.UnlockBits( bd );
+            }
+
+            return res;
+        }
+    }
+}
+
--- lab-08/Program.cs
+++ lab-08/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.IO;
-using System.Drawing;
 using System.Drawing.Imaging;
 
 namespace lab08 {
@@ -20,66 +19,6 @@ namespace lab08 {
     }
 
     class MainClass {
-        public static Bitmap LoadBitmap(string fileName) {
-            using (FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.Read))
-                return new Bitmap(fs);
-        }
-
-        public static byte[][,] CreateRgbBitmap( int height, int width ) {
-            return new byte[ 3 ][,] { new byte[ height, width ], new byte[ height, width ], new byte[ height, width ] };
-        }
-
-        public unsafe static byte[][,] BitmapToBytes( Bitmap bmp ) {
-            int width = bmp.Width, height = bmp.Height, h, w;
-            byte[][,] res = CreateRgbBitmap( height, width );
-            BitmapData bd = bmp.LockBits( new Rectangle( 0, 0, width, height ), ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb );
-
-            try {
-                byte* curpos;
-                for( h = 0; h < height; h++ ) {
-                    curpos = ( ( byte * )bd.Scan0 ) + h * bd.Stride;
-                    for( w = 0; w < width; w++ ) {
-                        res[ 2 ][ h, w ] = *( curpos++ );
-                        res[ 1 ][ h, w ] = *( curpos++ );
-                        res[ 0 ][ h, w ] = *( curpos++ );
-                    }
-                }
-            }
-            finally {
-                bmp.UnlockBits( bd );
-            }
-            return res;
-        }
-
-        public unsafe static Bitmap BytesToBitmap( byte[][,] rgb ) {
-            if( 3 != rgb.GetLength( 0 ) )
-                throw new ArrayTypeMismatchException( "Size of first dimension for passed array must be 3 (RGB components)." );
-            
-            if( 2 != rgb[ 0 ].Rank )
-                throw new ArrayTypeMismatchException( "Every passed component array should have 2 dimensions." );
-
-            int height = rgb[ 0 ].GetLength( 0 ), width = rgb[ 0 ].GetLength( 1 ), h, w;
-            Bitmap res = new Bitmap( width, height, PixelFormat.Format24bppRgb );
-            BitmapData bd = res.LockBits( new Rectangle( 0, 0, width, height ), ImageLockMode.WriteOnly, PixelFormat.Format24bppRgb );
-
-            try {
-                byte* curpos;
-                for( h = 0; h < height; h++ ) {
-                    curpos = ( ( byte * )bd.Scan0 ) + h * bd.Stride;
-                    for( w = 0; w < width; w++ ) {
-                        *( curpos++ ) = rgb[ 2 ][ h, w ];
-                        *( curpos++ ) = rgb[ 1 ][ h, w ];
-                        *( curpos++ ) = rgb[ 0 ][ h, w ];
-                    }
-                }
-            }
-            finally {
-                res.UnlockBits( bd );
-            }
-
-            return res;
-        }
-
         public static bool ReadArguments( ref string[] args, out string matrixFile, out string inFileName, out string outFormatExt, out string outFileName ) {
             matrixFile = inFileName = outFormatExt = outFileName = "";
 
@@ -125,8 +64,8 @@ namespace lab08 {
                 "Output image file: " + outFileName + "\n" +
                 "Convolutional matrix to apply:\n" + mtx );
 
-            byte[][,] bitmap = BitmapToBytes( LoadBitmap( inFileName ) );
-            byte[][,] new_bitmap = CreateRgbBitmap( bitmap[ 0 ].GetLength( 0 ), bitmap[ 0 ].GetLength( 1 ) );
+            byte[][,] bitmap = BitmapsWork.BitmapToBytes( BitmapsWork.LoadBitmap( inFileName ) );
+            byte[][,] new_bitmap = BitmapsWork.CreateRgbBitmap( bitmap[ 0 ].GetLength( 0 ), bitmap[ 0 ].GetLength( 1 ) );
             int c, m, n;
 
             for( c = 0; c < 3; c++)
@@ -134,7 +73,7 @@ namespace lab08 {
                     for( n = 0; n < bitmap[ c ].GetLength( 1 ); n++ )
                         new_bitmap[ c ][ m, n ] = ( byte )mtx.Apply( ref bitmap[ c ], m, n );
 
-            BytesToBitmap( new_bitmap ).Save( outFileName, ImageFormat.Bmp );
+            BitmapsWork.BytesToBitmap( new_bitmap ).Save( outFileName, ImageFormat.Bmp );
             return 0;
         }
     }
