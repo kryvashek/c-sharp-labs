039f530038eaf3201bedd1147ba7d56029acdec4
kryvashek <kryvashek@gmail.com>
2017-12-23 14:50:29 +0200

    Implemented GPX XML reader.
    Signed-off-by: kryvashek <kryvashek@gmail.com>

---
+++ lab-10/GpxPoint.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace lab10 {
+    public class GpxPoint {
+        readonly XElement _entry;
+        readonly DateTime _moment;
+        readonly double _latitude, _longitude, _altitude, _hdop;
+        readonly XNamespace _xmlns;
+        double _velocity, _distance;
+
+        public double Latitude { get { return _latitude; } }
+
+        public double Longitude { get { return _longitude; } }
+
+        public double Altitude { get { return _altitude; } }
+
+        public double Hdop { get { return _hdop; } }
+
+        public double Velocity {
+            get { return _velocity; }
+            set {
+                _velocity = value;
+
+                if( null == _entry.Element( _xmlns + "extensions" ) )
+                    _entry.Add( new XElement( _xmlns + "extensions" ) );
+
+                _entry.Element( _xmlns + "extensiosn" ).SetElementValue( _xmlns + "velocity", value );
+            }
+        }
+
+        public double Distance {
+            get { return _distance; }
+            set {
+                _distance = value;
+
+                if( null == _entry.Element( _xmlns + "extensions" ) )
+                    _entry.Add( new XElement( _xmlns + "extensions" ) );
+
+                _entry.Element( _xmlns + "extensiosn" ).SetElementValue( _xmlns + "distance", value );
+            }
+        }
+
+        public GpxPoint( double latitude, double longitude, DateTime moment, double altitude = .0, double hdop = .0 ) {
+            _latitude = latitude;
+            _longitude = longitude;
+            _moment = moment;
+            _altitude = altitude;
+            _hdop = hdop;
+            _xmlns = XNamespace.Get( @"http://www.topografix.com/GPX/1/1" );
+            _velocity = .0;
+            _distance = .0;
+            _entry = new XElement( _xmlns + "trkpt" );
+            _entry.SetAttributeValue( _xmlns + "lat", _latitude );
+            _entry.SetAttributeValue( _xmlns + "lon", _longitude );
+            _entry.SetElementValue( _xmlns + "ele", _altitude );
+            _entry.SetElementValue( _xmlns + "hdop", _hdop );
+            _entry.SetElementValue( _xmlns + "time", _moment.ToUniversalTime() );
+        }
+
+        private bool ParseDouble( string text, out double value ) {
+            return double.TryParse( text.Replace( '.', ',' ), out value );
+        }
+
+        private double ReadDoubleAttr( XElement entry, XName name ) {
+            XAttribute attr = entry.Attribute( name );
+            double temp;
+
+            if( null == attr )
+                throw new ArgumentOutOfRangeException( "No '" + name + "' attribute passed to GpxPoint in XML entry:\n" + entry );
+
+            if( !ParseDouble( attr.Value, out temp ) )
+                throw new ArgumentOutOfRangeException( "Wrong value of '" + name + "' attribute ('" + attr.Value + "') passed to GpxPoint via XML." );
+               
+            return temp;
+        }
+
+        private double ReadDoubleField( XElement entry, XName name ) {
+            XElement field = entry.Element( name );
+            double temp;
+
+            if( null != field && ParseDouble( field.Value, out temp ) )
+                return temp;
+
+            return .0;
+        }
+
+        public GpxPoint( XElement entry, XNamespace xmlns ) {
+            _entry = entry;
+            _xmlns = xmlns;
+            _latitude = ReadDoubleAttr( _entry, "lat" );
+            _longitude = ReadDoubleAttr( _entry, "lon" );
+            _altitude = ReadDoubleField( _entry, _xmlns + "ele" );
+            _hdop = ReadDoubleField( _entry, _xmlns + "hdop" );
+
+            XElement timeEntry = _entry.Element( _xmlns + "time" );
+
+            if( null == timeEntry )
+                throw new ArgumentOutOfRangeException( "No 'time' element passed to GpxPoint in XML entry:\n" + entry );
+
+            if( !DateTime.TryParse( timeEntry.Value, out _moment ) )
+                throw new ArgumentOutOfRangeException( "Wrong value of 'time' element ('" + timeEntry.Value + "') passed to GpxPoint via XML." );
+        }
+
+        override public string ToString() {
+            return _latitude + ":" + _longitude + ":" + _altitude + " on " + _moment;
+        }
+    }
+}
+
---
+++ lab-10/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace lab10 {
+    class MainClass {
+        private static XmlReader CreateReader( ref object source ) {
+            if( source is string )
+                return XmlReader.Create( source as string );
+            else if( source is TextReader )
+                return XmlReader.Create( source as TextReader );
+            else if( source is Stream )
+                return XmlReader.Create( source as Stream );
+            else if( source is XElement )
+                return ( source as XElement ).CreateReader();
+            else if( source is XmlReader )
+                return ( source as XmlReader );
+            else
+                return null;
+        }
+
+        private delegate bool CheckerMethod( string name );
+
+        private static CheckerMethod CreateChecker( ref object checkData ) {
+            if( checkData is string )
+                return ( checkData as string ).Equals;
+            else if( checkData is ICollection< string > )
+                return ( checkData as ICollection< string > ).Contains;
+            else
+                return null;
+        }
+
+        static IEnumerable< XmlReader > ReadEntries( object source, object checkData ) {
+            using( XmlReader reader = CreateReader( ref source ) ) {
+                CheckerMethod checker = CreateChecker( ref checkData );
+                reader.MoveToContent();
+
+                while( reader.Read() )
+                    if( XmlNodeType.Element == reader.NodeType && checker( reader.Name ) )
+                        yield return reader.ReadSubtree();
+            }
+        }
+
+        static XElement ReadElement( XmlReader reader ) {
+            reader.MoveToContent();
+            return ( XElement.ReadFrom( reader ) as XElement );
+        }
+
+        public static void Main( string[] args ) {
+            using( XmlReader reader = XmlReader.Create( "/home/kryvashek/SampleTrack.gpx" ) ) {
+                foreach( XmlReader trkReader in ReadEntries( reader, "trk" ) )
+                    foreach( XmlReader trksegReader in ReadEntries( trkReader, "trkseg" ) )
+                        foreach( XmlReader trkptReader in ReadEntries( trksegReader, "trkpt" ) )
+                            Console.WriteLine( new GpxPoint( ReadElement( trkptReader ), reader.NamespaceURI ) );
+            }
+        }
+    }
+}
